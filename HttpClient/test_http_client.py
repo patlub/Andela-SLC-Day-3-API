679a6ec13b25d9b68c1c1f14ddaac74988113517
Patrick Luboobi <patricklu2010@gmail.com>
2017-07-05 15:41:35 +0300

    Refactor test cases

--- HttpClient/test_http_client.py
+++ HttpClient/test_http_client.py
@@ -4,20 +4,22 @@ from pip._vendor import requests
 
 from HttpClient.http_client import sources, fetch_news
 
-class HttpClientTestCase(unittest.TestCase):
 
+class HttpClientTestCase(unittest.TestCase):
     def test_news_sources(self):
         self.assertEquals(sources(),
-                          ['techcrunch', 'techradar', 'ars-technica'])
+                          ['techcrunch', 'techradar', 'ars-technica'],
+                          msg='Should return the accepted news sources')
 
     def test_fetch_news_from_invalid_source(self):
-        self.assertEquals(fetch_news('Kampala'),
-                          'Invalid Source')
+        self.assertEquals(fetch_news('Kampala'), 'Invalid Source',
+                          msg='Should return invalid Source for unknown source')
 
     def test_api_call_is_valid(self):
         api_key = '51ffb1a860b7437cbd213e7e16c1c5c7'
         source = 'techcrunch'
-        response = requests.get('https://newsapi.org/v1/articles?source=' + source + '&apiKey=' + api_key)
+        response = requests.get('https://newsapi.org/v1/articles?source='
+                                + source + '&apiKey=' + api_key)
         self.assertTrue(response.ok)
 
 
